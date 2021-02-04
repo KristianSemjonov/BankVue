1160b7b6444990fbf78d468dffeec54bcfd136e8
Kristian Semjonov <kristian.semjonov@gmail.com>
2021-02-04 10:51:32 +0200

    neljapäev

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    First name: <input v-model="firstName" placeholder="first name">
-    Last name: <input v-model="lastName" placeholder="last name">
-    <button v-on:click="saveInHtml()">Save</button>
+    Account: <input v-model="accountBalance" placeholder="number">
+    <button v-on:click="getData()">Submit</button>
+    <table border="1">
+      <tr>
+        <th>id</th>
+        <th>customer_id</th>
+        <th>account_nr</th>
+        <th>balance</th>
+      </tr>
+      <tr v-for="row in account">
+        <td>{{row.id}}</td>
+        <td>{{row.customer_id}}</td>
+        <td>{{row.account_nr}}</td>
+        <td>{{row.balance}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+let getData = function(){
+  this.$http.get('http://localhost:8081/bank2/accountBalance?accountNr=' + this.accountBalance)
+  .then(response => this.accountBalance = response.data)
+  .catch(response => console.log(response))
+}
 // @ is an alias to /src
 let saveInJs = function () {
-  this.$http.get('http://localhost:8080/test')
+  this.$http.get('http://localhost:8081/bank2/accountBalance?accountNr=', this.accountBalance);
   alert('Info is saved');
 }
 
@@ -19,13 +37,17 @@ export default {
   components: {},
   data: function () {
     return {
-      firstName: '',
-      lastName: ''
+      accountBalance: '',
+      account: []
     }
   },
   methods:
       {
-        saveInHtml: saveInJs
-      }
+        saveInHtml: saveInJs,
+        getData: getData
+      },
+  mounted (){
+
+  }
 }
 </script>
