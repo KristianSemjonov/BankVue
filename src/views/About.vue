a5bda98d7343aed954fa5f30dac6d95c4794765a
Kristian Semjonov <kristian.semjonov@gmail.com>
2021-02-15 10:33:05 +0200

    esmaspäev 10.32

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is a testbank to learn and test Vue functionality</h1>
+    <p>Hi Linda, tereeee</p>
   </div>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,51 +1,51 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Login</h1>
-    Username: <input v-model="username" placeholder="">
-    Password: <input v-model="password" placeholder="">
-    <button v-on:click="saveInHtml()">Submit</button>
-  </div>
-</template>
+<!--<template>-->
+<!--  <div class="home">-->
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <h1>Login</h1>-->
+<!--    Username: <input v-model="username" placeholder="">-->
+<!--    Password: <input v-model="password" placeholder="">-->
+<!--    <button v-on:click="saveInHtml()">Submit</button>-->
+<!--  </div>-->
+<!--</template>-->
 
-<script>
-// let getData = function(){
-//   this.$http.get('http://localhost:8081/bank2/accountBalance?accountNr=' + this.accountBalance)
-//   .then(response => this.accountBalance = response.data)
-//   .catch(response => console.log(response))
-// }
-// @ is an alias to /src
+<!--<script>-->
+<!--// let getData = function(){-->
+<!--//   this.$http.get('http://localhost:8081/bank2/accountBalance?accountNr=' + this.accountBalance)-->
+<!--//   .then(response => this.accountBalance = response.data)-->
+<!--//   .catch(response => console.log(response))-->
+<!--// }-->
+<!--// @ is an alias to /src-->
 
-//http://localhost:8081/bank2/createAccount?accountNr=EE123&customer_id=1
-let saveInJs = function () {
-  this.$http.get('http://localhost:8081/bank2/createAccount',
-      {
-        params: {
-          accountNr: this.customer.account,
-          customer_id: this.customer.id
-        }
+<!--//http://localhost:8081/bank2/createAccount?accountNr=EE123&customer_id=1-->
+<!--let saveInJs = function () {-->
+<!--  this.$http.get('http://localhost:8081/bank2/createAccount',-->
+<!--      {-->
+<!--        params: {-->
+<!--          accountNr: this.customer.account,-->
+<!--          customer_id: this.customer.id-->
+<!--        }-->
 
-      })
-}
-//alert('Info is saved');
+<!--      })-->
+<!--}-->
+<!--//alert('Info is saved');-->
 
 
-export default {
-  name: 'Account',
-  components: {},
-  data: function () {
-    return {
-      customer: {},
-      accounts: []
-    }
-  },
-  methods:
-      {
-        saveInHtml: saveInJs,
-        getData: getData
-      },
-  mounted() {
+<!--export default {-->
+<!--  name: 'Account',-->
+<!--  components: {},-->
+<!--  data: function () {-->
+<!--    return {-->
+<!--      customer: {},-->
+<!--      accounts: []-->
+<!--    }-->
+<!--  },-->
+<!--  methods:-->
+<!--      {-->
+<!--        saveInHtml: saveInJs,-->
+<!--        getData: getData-->
+<!--      },-->
+<!--  mounted() {-->
 
-  }
-}
-</script>
+<!--  }-->
+<!--}-->
+<!--</script>-->
